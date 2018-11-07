18fd459f34b99f839c85136c873877a3f581470d
Paul Ngo <43042992+ngovidangkhoa@users.noreply.github.com>
2018-11-07 12:14:30 -0500

    dasdasds

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,80 @@
 function test1(){
 
 	var fName = prompt("What is your name?");
-	var myColour = ("Pick a colour " + fName);
-	
+	var myColour = prompt ("Pick a colour, " + fName);
 
-
-
+	if(myColour == "red") {
+		console.log ("hahahahahaha mew mewwwwwwwwwww like red?, u suck!");
+	}
+	else if(myColour == "green"){
+		console.log ("So? u still suck af!!!");
+	}
 }
+
+
+function test2(){
+
+	var age = prompt ("How old are you?");
+	if(age < 14){
+		alert ("that's not too old");
+	}
+
+	else if (age = 14){
+		alert ("You are older than I thought");
+
+	}
+
+	else if (age>14){
+		alert ("14 is a good age");
+		}
+
+		else { 
+		alert ("Not a vaild age");
+
+				}	
+}
+
+
+function test3(){
+
+		var number = prompt ("Enter the number");
+		if (number < 0){
+			alert ("this is a negative number");
+		}
+
+		else if (number > 0){
+			alert ("this a a positive number");
+		}
+
+		else if (number = 0){
+			alert ("just a 0");
+		}
+
+	}
+
+
+function FizzBuzz(){
+	for ( var i=1; i <=100; i++) 
+	{
+		if (i%3 ===0 && i%5 ===0){
+			console.log (i+"FizzBuzz");
+		}
+
+		else if (i%3 === 0){
+			console.log( i+"Fizz");
+		}
+
+		else if (i%5 === 0){
+			console.log (i+"Buzz");
+		}
+
+		else
+			{
+				console.log(i);
+			}
+		}
+}
+
+
+
+
---
+++ JS/sub.js
@@ -0,0 +1,27 @@
+function pattern(){
+	
+	for (var x=1; x<=6; x++)
+	{
+		if (x==1)
+		{
+			console.log (1 + "'*'");
+		}
+		else if (x==2)
+		{
+			console.log (2 +"'*'");
+		}
+		else if (x==3)
+		{
+			console.log (3+ "'*'");
+		}
+		else if (x==4)
+		{
+			console.log (4+"'*'");
+		}
+		else (x==6)
+		{
+			console.log (6+"'*'");
+		}
+	
+	}
+}

aa6e29254c6e4e88ffe2e0ad8b3747aaaf981878
Paul Ngo <43042992+ngovidangkhoa@users.noreply.github.com>
2018-12-06 14:09:43 -0500

    sda

--- JS/ProgramTestJS.js
+++ JS/ProgramTestJS.js
@@ -1,5 +1,3 @@
-Program Test JS
-
 function A6(plaintext){
 
 var ciphertext="";
@@ -10,18 +8,18 @@ for(var i=0; i<wordlist.length; i++){
 
 if(wordlist[i].length > 2){
 
-ciphertext = ciphertext + wordlist[i].substring(1,) + wordlist[i][0]+"ay ";
+ciphertext = ciphertext + wordlist[i].substring(1, ) + wordlist[i][0]+ " cts " + wordlist[i].toUpperCase() + " Fish! ";
 
 } else {
 
-ciphertext = ciphertext + wordlist[i] + " ";
+ciphertext = ciphertext + wordlist[i] + "tsc " + "fake";
 
 }
 
 }
 
-return (ciphertext);
+return ("tsccts " + ciphertext + "tsccts");
 
 }
 
-console.log(A6("hello my name is simon"));
+console.log(A6("Fish is fish, but dog is not a fish"));
